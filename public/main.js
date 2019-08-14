c39c333b04b4ed61fcf7449ad51edbddf66c543b
Adam Jones <adam@linuxtampa.com>
2019-08-13 20:27:56 -0400

    Added 4 weather elements with styling and a background image

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
+let result = ''
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  const url = 'http://api.openweathermap.org/data/2.5/weather?q='
+  const city = document.querySelector('#input').value
+  const apikey = '&APPID=3434bc4513b87fa0bd2d62558903e72b'
+  const units = '&units=imperial'
+  result = url + city + apikey + units
+  console.log(result)
+  // fetch to get the data
+  fetch(result)
+    .then(resp => {
+      console.log(resp)
+      return resp.json()
+    })
+    .then(data => {
+      console.log(data)
+      document.querySelector('#temp').textContent = data.main.temp
+      document.querySelector('#humid').textContent = data.main.humidity
+      document.querySelector('#high-temp').textContent = data.main.temp_max
+      document.querySelector('#low-temp').textContent = data.main.temp_min
+    })
 }
-
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.search-button').addEventListener('click', main)
