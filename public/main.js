fb5f555a06f83c669cc82173af5728ac04f44fcb
Adam Jones <adam@linuxtampa.com>
2019-09-23 19:02:37 -0400

    Added more styling and pressure reading

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
 let result = ''
 const main = () => {
   const url = 'http://api.openweathermap.org/data/2.5/weather?q='
-  const city = document.querySelector('#input').value
+  const zip = document.querySelector('#input').value
   const apikey = '&APPID=3434bc4513b87fa0bd2d62558903e72b'
   const units = '&units=imperial'
-  result = url + city + apikey + units
+  result = url + zip + apikey + units
   console.log(result)
   // fetch to get the data
   fetch(result)
@@ -14,10 +14,18 @@ const main = () => {
     })
     .then(data => {
       console.log(data)
-      document.querySelector('#temp').textContent = data.main.temp
+      document.querySelector('#temp').textContent = Math.round(data.main.temp)
       document.querySelector('#humid').textContent = data.main.humidity
-      document.querySelector('#high-temp').textContent = data.main.temp_max
-      document.querySelector('#low-temp').textContent = data.main.temp_min
+      document.querySelector('#high-temp').textContent = Math.round(
+        data.main.temp_max
+      )
+      document.querySelector('#low-temp').textContent = Math.round(
+        data.main.temp_min
+      )
+      document.querySelector('#pressure').textContent = Math.round(
+        data.main.pressure
+      )
+      document.querySelector('#wind').textContent = Math.round(data.wind.speed)
     })
 }
 document.querySelector('.search-button').addEventListener('click', main)
